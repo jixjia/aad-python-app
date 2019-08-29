32dc476a1138ffc8c91cd8138471e6a6b625bed3
Jixin Jia <jixjia@microsoft.com>
2019-08-29 23:31:34 +0900

    Added login

--- app.py
+++ app.py
@@ -1,11 +1,70 @@
-from flask import Flask
-app = Flask(__name__)
+import adal
+import flask
+import uuid
+import requests
+import config
+
+app = flask.Flask(__name__)
+app.secret_key = 'development'
+
+AUTHORITY_URL = config.AUTHORITY_HOST_URL + '/' + config.TENANT
+HOST = 'https://boav5.azurewebsites.net'
+# HOST = 'http://localhost:5000'
+REDIRECT_URI = HOST + '/getAToken'
+TEMPLATE_AUTHZ_URL = ('https://login.microsoftonline.com/{}/oauth2/authorize?' +
+                      'response_type=code&client_id={}&redirect_uri={}&' +
+                      'state={}&resource={}')
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def main():
+    login_url = HOST + '/login'
+    resp = flask.Response(status=307)
+    resp.headers['location'] = login_url
+    return resp
 
 
-if __name__ == '__main__':
+@app.route("/login")
+def login():
+    auth_state = str(uuid.uuid4())
+    flask.session['state'] = auth_state
+    authorization_url = TEMPLATE_AUTHZ_URL.format(
+        config.TENANT,
+        config.CLIENT_ID,
+        REDIRECT_URI,
+        auth_state,
+        config.RESOURCE)
+    resp = flask.Response(status=307)
+    resp.headers['location'] = authorization_url
+    return resp
+
+
+@app.route("/getAToken")
+def main_logic():
+    code = flask.request.args['code']
+    auth_context = adal.AuthenticationContext(AUTHORITY_URL)
+    token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, config.RESOURCE,
+                                                                        config.CLIENT_ID, config.CLIENT_SECRET)
+    # It is recommended to save this to a database when using a production app.
+    flask.session['access_token'] = token_response['accessToken']
+
+    return flask.redirect('/userProfile')
+
+
+@app.route('/userProfile')
+def graphcall():
+    if 'access_token' not in flask.session:
+        return flask.redirect(flask.url_for('login'))
+    endpoint = config.RESOURCE + '/' + config.API_VERSION + '/me/'
+    http_headers = {'Authorization': 'Bearer ' + flask.session.get('access_token'),
+                    'User-Agent': 'adal-python-sample',
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'client-request-id': str(uuid.uuid4())}
+    graph_data = requests.get(
+        endpoint, headers=http_headers, stream=False).json()
+    return flask.render_template('graph_info.html', graph_data=graph_data)
+
+
+if __name__ == "__main__":
     app.run()
