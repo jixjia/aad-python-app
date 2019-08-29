7641adf9c9941cf37376bc68f8b2d8b9946824f4
Jixin Jia <jixjia@microsoft.com>
2019-08-29 18:50:15 +0900

    Stupid Azure

---
+++ config.py
@@ -0,0 +1,11 @@
+# Add the resource you want the access token for
+RESOURCE = "https://graph.microsoft.com"
+
+# Enter tenant name, e.g. contoso.onmicrosoft.com
+TENANT = "microsoft.onmicrosoft.com"
+AUTHORITY_HOST_URL = 'https://login.microsoftonline.com'
+CLIENT_ID = '9a3801bd-92db-4307-8501-5fa0e22e5f5b '
+CLIENT_SECRET = 'a9.HN]]1uC]Wppl0Eu4+4fdv5Ka3XX9h'
+
+# These settings are for the Microsoft Graph API Call
+API_VERSION = 'v1.0'
